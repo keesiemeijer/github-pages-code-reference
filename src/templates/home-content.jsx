887bd721ec1740e8e19e2e452ec0536fc6a88252
Kees Meijer <keesie.meijer@gmail.com>
2018-06-11 15:18:49 +0200

    Update home template with new defaults

--- src/templates/home-content.jsx
+++ src/templates/home-content.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const HomeContent = props => {
 	return (
 		<div>
-			<h2>Welcome to the Related Posts by Taxonomy plugin code reference</h2>
+			<h2>Welcome to the Example Reference Name code reference</h2>
 <p>
-	Want to know what's going on inside this plugin.	Search the Code Reference for more information about functions, classes, methods, and hooks.</p>
+	Search the Code Reference for more information about functions, classes, methods, and hooks.</p>
 
 		</div>
 	)
