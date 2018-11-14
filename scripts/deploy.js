189dac1778569ca394e34d5e2090f71fb969ba19
Kees Meijer <keesie.meijer@gmail.com>
2018-11-14 16:53:07 +0100

    Update deploy messages

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,8 +1,18 @@
 var reference = require('../src/reference.json');
 var ghpages = require('gh-pages');
 
+console.log('Start deploy...');
 if (reference.repo_gh_pages) {
+	console.log('Deploying the reference to the gh-pages branch of the repository: ' + reference.repo_gh_pages);
 	ghpages.publish('build', {
 		repo: reference.repo_gh_pages
-	}, function(err) { console.log(err) });
+	}, function(err) {
+		if(err) {
+			console.log(err)
+		} else {
+			console.log('Deployed successfully');
+		}
+	});
+} else {
+	console.log('Error: Could not find the repo to deploy to');
 }
