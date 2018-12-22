2c1c788a4ef92c5a7218c22c9afd80d202b5fd65
Kees Meijer <keesie.meijer@gmail.com>
2018-12-22 13:53:26 +0100

    Add @see references in the description

--- child-theme/reference/template-description.php
+++ child-theme/reference/template-description.php
@@ -9,10 +9,38 @@
 namespace DevHub;
 ?>
 
-<?php if ( $description = get_description() ) : ?>
+<?php if ( $description = get_description() || $see = get_see_tags() ) : ?>
 	<hr />
 	<section class="description">
-		<h2><?php _e( 'Description', 'wporg' ); ?></h2>
-		<?php echo $description; ?>
+		<?php if ( $description ) : ?>
+			<h2><?php _e( 'Description', 'wporg' ); ?></h2>
+			<?php echo get_description(); ?>
+		<?php endif; ?>
+		<?php if ( $see ) : ?>
+				<h3><?php _e( 'See also', 'wporg' ); ?></h3>
+				<ul>
+				<?php
+				foreach ( $see as $tag ) {
+					$see_ref = '';
+					if ( ! empty( $tag['refers'] ) ) {
+						$see_ref .= '{@see ' . $tag['refers'] . '}';
+					}
+					if ( ! empty( $tag['content'] ) ) {
+						if ( $see_ref ) {
+							$see_ref .= ': ';
+						}
+						$see_ref .= $tag['content'];
+					}
+					// Process text for auto-linking, etc.
+					remove_filter( 'the_content', 'wpautop' );
+					$see_ref = apply_filters( 'the_content', apply_filters( 'get_the_content', $see_ref ) );
+					add_filter( 'the_content', 'wpautop' );
+
+					echo '<li>' . $see_ref . "</li>\n";
+				}
+				?>
+				</ul>
+
+		<?php endif; ?>
 	</section>
 <?php endif; ?>
